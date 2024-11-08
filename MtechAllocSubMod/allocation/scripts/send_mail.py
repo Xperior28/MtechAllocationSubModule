3a8cec8001361d37555571c5836ccad2be9d248a
Xperior28 <rohithraichu@gmail.com>
2024-11-08 14:55:50 +0530

    bug fix

--- MtechAllocSubMod/allocation/scripts/send_mail.py
+++ MtechAllocSubMod/allocation/scripts/send_mail.py
@@ -22,14 +22,14 @@ def send_clashmail(student_data, professor):
     # Send GET request to retrieve the selected student
     start_time = time.time()
     hello_count = 0
+    print("Listening for a response...")
     while True:
         time.sleep(1)
-        print("buffer")
-
+        
         # Check if 10 seconds have passed
         if time.time() - start_time >= 10 and hello_count < 2:
-            reponse = requests.post(deployment_url, json={'action': 'sendReminder'})
-            print(reponse.text)
+            response = requests.post(deployment_url, json={'action': 'sendReminder'})
+            print(response.text)
             hello_count += 1
             start_time = time.time()  # Reset the timer
             if hello_count == 2:
@@ -37,7 +37,8 @@ def send_clashmail(student_data, professor):
 
         payload = {'action': 'getFormResponse'}
         response = requests.post(deployment_url, json=payload)
-        print("Form Response -> ", response.text)  # This will print the selected student
+        if response.text != 'No responses found.':
+           print("Form Response -> ", response.text)  # This will print the selected student
         if response.text == "No responses found.":
             continue
         else:
